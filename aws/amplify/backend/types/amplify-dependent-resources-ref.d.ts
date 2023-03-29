702cc903ae30d024987cd3c33aba7f56ba7f7d2a
Alan Jiang <adj2424@gmail.com>
2023-03-28 23:14:51 -0400

    sus

--- aws/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ aws/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,18 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "fetchData": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "fetchData": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.tsx
+++ src/App.tsx
@@ -4,37 +4,42 @@ import Graph from './Graph';
 import Table from './Table';
 
 function App() {
-  const [originalTimeline, setOriginalTimeline] = useState([]);
-  const [timeline, setTimeline] = useState([]);
-  const [account, setAccount] = useState({});
+	const [originalTimeline, setOriginalTimeline] = useState([]);
+	const [timeline, setTimeline] = useState([]);
+	const [account, setAccount] = useState({});
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const url = 'http://127.0.0.1:5001/algosus/us-central1/fetch';
-      await fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          setAccount(data.account);
-          //setBuy(data.buy);
-          //setSell(data.sell);
-          setTimeline(Object.values(data.timeline));
-          setOriginalTimeline(Object.values(data.timeline));
-        })
-        .catch(err => console.log(err));
-    };
-    fetchData();
-  }, []);
+	const test = async () => {
+		await fetch('');
+	};
 
-  return (
-    <div className="flex-container">
-      <div className="graph">
-        <Graph original={originalTimeline} timeline={timeline} setTimeline={setTimeline} />
-      </div>
-      <div className="table">
-        <Table account={account} />
-      </div>
-    </div>
-  );
+	useEffect(() => {
+		const fetchData = async () => {
+			const url = 'http://127.0.0.1:5001/algosus/us-central1/fetch';
+			await fetch(url)
+				.then(response => response.json())
+				.then(data => {
+					setAccount(data.account);
+					//setBuy(data.buy);
+					//setSell(data.sell);
+					setTimeline(Object.values(data.timeline));
+					setOriginalTimeline(Object.values(data.timeline));
+				})
+				.catch(err => console.log(err));
+		};
+		fetchData();
+	}, []);
+
+	return (
+		<div className="flex-container">
+			<button onClick={test}>hi</button>
+			<div className="graph">
+				<Graph original={originalTimeline} timeline={timeline} setTimeline={setTimeline} />
+			</div>
+			<div className="table">
+				<Table account={account} />
+			</div>
+		</div>
+	);
 }
 
 export default App;
