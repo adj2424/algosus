a491b234014904d8569c9455dfc84255e70c3325
Alan Jiang <adj2424@gmail.com>
2023-04-01 00:08:07 -0400

    fixed button

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,6 @@ function App() {
 	const [timeline, setTimeline] = useState([]);
 	const [account, setAccount] = useState({});
 
-	const test = async () => {
-		await fetch('https://dvjl0e8mw9.execute-api.us-east-1.amazonaws.com/dev/fetch')
-			.then(res => res.json())
-			.then(data => console.log(data));
-	};
-
 	useEffect(() => {
 		const fetchData = async () => {
 			const url = 'http://127.0.0.1:5001/algosus/us-central1/fetch';
@@ -33,7 +27,6 @@ function App() {
 
 	return (
 		<div className="flex-container">
-			<button onClick={test}>hi</button>
 			<div className="graph">
 				<Graph original={originalTimeline} timeline={timeline} setTimeline={setTimeline} />
 			</div>
